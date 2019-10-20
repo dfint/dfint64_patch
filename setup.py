d085aa81d0c69e1dbe2973c049ea1ab01941e8d4
insolor <insolor@gmail.com>
2019-10-20 19:55:25 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='dfrus64',
+      version='0.0.1',
+      # description='',
+      url='https://github.org/dfint/dfrus64',
+      author='insolor',
+      author_email='insolor@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
