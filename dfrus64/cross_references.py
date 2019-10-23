b25785d50a3b190e3113d3f53f093001b6340978
insolor <insolor@gmail.com>
2019-10-23 09:56:17 +0300

    Implemented basic analysis in the patch.py
    Implemented basic analysis in the patch.py
    
    Replace reduce with chain

--- dfrus64/cross_references.py
+++ dfrus64/cross_references.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
-from typing import List, Mapping
+from itertools import chain
+from typing import List, Mapping, Tuple, Iterable
 
 from dfrus64.type_aliases import Rva
 
 
-def find_relative_cross_references(bytes_block: bytes, base_address: Rva, addresses: List[Rva]) -> Mapping[Rva, List[Rva]]:
+def find_relative_cross_references(bytes_block: bytes, base_address: Rva, addresses: Iterable[Rva]) \
+        -> Mapping[Rva, List[Rva]]:
     """
     Analyse a block of bytes and try to find relative cross references to the given objects' addresses
     :param bytes_block: bytes block to analyze
@@ -25,3 +27,22 @@ def find_relative_cross_references(bytes_block: bytes, base_address: Rva, addres
             result[destination].append(base_address + i)
 
     return result
+
+
+def invert_cross_reference_table(cross_references: Mapping[Rva, List[Rva]]) -> Mapping[Rva, Rva]:
+    result = dict()
+    for object_rva, references in cross_references.items():
+        for ref in references:
+            result[ref] = object_rva
+
+    return result
+
+
+def find_intersected_cross_references(cross_references: Mapping[Rva, List[Rva]]) -> Iterable[Tuple[Rva, Rva]]:
+    references = sorted(chain.from_iterable(cross_references.values()))
+
+    for i, item in enumerate(references):
+        j = i + 1
+        while j < len(references) and references[j] - item < 4:
+            yield item, references[j]
+            j += 1
--- dfrus64/patch.py
+++ dfrus64/patch.py
@@ -1,14 +1,52 @@
+import mmap
+import sys
 
-from .cross_references import find_relative_cross_references
+from .cross_references import *
 from .extract_strings import extract_strings_from_raw_bytes
 
 import click
-import pefile
+from pefile import PE
 
 
+@click.command()
 @click.argument('file_name')
 def main(file_name):
-    pass
+    with open(file_name, 'rb') as pe_file:
+        file_data = mmap.mmap(pe_file.fileno(), 0, access=mmap.ACCESS_READ)
+        pe = PE(data=file_data, fast_load=True)
+
+        code_section = pe.sections[0]
+        string_section = pe.sections[1]
+
+        image_base = pe.OPTIONAL_HEADER.ImageBase
+
+        print("Extracting strings... ")
+        strings = extract_strings_from_raw_bytes(string_section.get_data(),
+                                                 base_address=string_section.VirtualAddress + image_base)
+
+        print('Found', len(strings), 'string-like objects')
+
+        print('Searching for cross references...')
+        cross_references = find_relative_cross_references(code_section.get_data(),
+                                                          base_address=code_section.VirtualAddress + image_base,
+                                                          addresses=strings.keys())
+
+        object_rva_by_reference_rva = invert_cross_reference_table(cross_references)
+
+        print('Among them', len(cross_references), 'objects with references from code section')
+        print('In total', sum(map(len, cross_references.values())), 'cross references')
+
+        print("Searching intersections in the cross references...")
+
+        intersections = find_intersected_cross_references(cross_references)
+
+        for ref1, ref2 in intersections:
+            obj1_rva = object_rva_by_reference_rva[ref1]
+            obj2_rva = object_rva_by_reference_rva[ref2]
+            print('0x{:x} (to 0x{:x} {!r}) / '
+                  '0x{:x} (to 0x{:x} {!r})'
+                  .format(ref1, obj1_rva, strings[obj1_rva],
+                          ref2, obj2_rva, strings[obj2_rva]))
 
 
 if __name__ == '__main__':
