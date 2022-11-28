f78b60200c0cc9bca164f3ce34619b65ba288065
insolor <insolor@gmail.com>
2022-11-28 19:53:42 +0300

    Move extract_strings.py into a separate package

---
+++ dfrus64/extract_strings/cli.py
@@ -0,0 +1,53 @@
+import operator
+import sys
+
+import click
+from loguru import logger
+from peclasses.portable_executable import PortableExecutable
+
+from dfrus64.cross_references import find_relative_cross_references
+from dfrus64.extract_strings.extract_strings import extract_strings_from_raw_bytes
+
+
+@click.command()
+@click.argument("file_name")
+@click.argument("out_file", default=None)
+def main(file_name, out_file):
+    with open(file_name, "rb") as pe_file:
+        pe = PortableExecutable(pe_file)
+
+        sections = pe.section_table
+        code_section = sections[0]
+        string_section = sections[1]
+
+        image_base = pe.optional_header.image_base
+
+        if out_file is None:
+            file = sys.stdout
+        else:
+            file = open(out_file, "w")
+
+        strings = list(
+            extract_strings_from_raw_bytes(
+                string_section.get_data(),
+                base_address=string_section.VirtualAddress + image_base,
+            )
+        )
+
+        cross_references = find_relative_cross_references(
+            code_section.get_data(),
+            base_address=code_section.VirtualAddress + image_base,
+            addresses=map(operator.itemgetter(0), strings),
+        )
+
+        for address, string in sorted(strings, key=operator.itemgetter(0)):
+            # Only objects with references from the code
+            if address in cross_references:
+                logger.info(hex(address), string, file=file)
+
+        if out_file is None:
+            file.close()
+
+
+if __name__ == "__main__":
+    main()
--- dfrus64/extract_strings/extract_strings.py
+++ dfrus64/extract_strings/extract_strings.py
@@ -1,12 +1,5 @@
-import operator
-import sys
 from typing import Iterator, Tuple, Union
 
-import click
-from loguru import logger
-from peclasses.portable_executable import PortableExecutable
-
-from dfrus64.cross_references import find_relative_cross_references
 from dfrus64.type_aliases import Rva
 
 forbidden = set(b"$^")
@@ -74,47 +67,3 @@ def extract_strings_from_raw_bytes(
             continue
 
         i += alignment
-
-
-@click.command()
-@click.argument("file_name")
-@click.argument("out_file", default=None)
-def main(file_name, out_file):
-    with open(file_name, "rb") as pe_file:
-        pe = PortableExecutable(pe_file)
-
-        sections = pe.section_table
-        code_section = sections[0]
-        string_section = sections[1]
-
-        image_base = pe.optional_header.image_base
-
-        if out_file is None:
-            file = sys.stdout
-        else:
-            file = open(out_file, "w")
-
-        strings = list(
-            extract_strings_from_raw_bytes(
-                string_section.get_data(),
-                base_address=string_section.VirtualAddress + image_base,
-            )
-        )
-
-        cross_references = find_relative_cross_references(
-            code_section.get_data(),
-            base_address=code_section.VirtualAddress + image_base,
-            addresses=map(operator.itemgetter(0), strings),
-        )
-
-        for address, string in sorted(strings, key=operator.itemgetter(0)):
-            # Only objects with references from the code
-            if address in cross_references:
-                logger.info(hex(address), string, file=file)
-
-        if out_file is None:
-            file.close()
-
-
-if __name__ == "__main__":
-    main()
