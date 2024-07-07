ee8fd081a81c55875b08f1d2a36320b8c6d22b64
insolor <2442833+insolor@users.noreply.github.com>
2024-07-07 17:12:26 +0300

    Get strings from exe output

--- tests/test_extract_strings.py
+++ tests/test_extract_strings.py
@@ -1,5 +1,7 @@
 import contextlib
 import io
+import platform
+import subprocess
 import tempfile
 from pathlib import Path
 
@@ -10,6 +12,8 @@ from dfint64_patch.extract_strings.from_raw_bytes import (
     check_string,
     extract_strings_from_raw_bytes,
 )
+
+tests_dir = Path(__file__).parent
 
 
 @pytest.mark.parametrize(
@@ -60,15 +64,30 @@ EXE_STRINGS = {
 }
 
 
-def test_extract_string_cli():
-    exe_file_path = Path(__file__).parent / "test64.exe"
+@pytest.fixture()
+def exe_strings() -> set[str]:
+    if platform.system() == "Windows":
+        result = subprocess.check_output([tests_dir / "test64.exe"], shell=False)  # noqa: S603
+    else:
+        result = subprocess.check_output(["wine", tests_dir / "test64.exe"], shell=False)  # noqa: S603, S607
+
+    return set(result.decode().splitlines())
+
+
+def test_exe_strings(exe_strings: set[str]):
+    assert exe_strings, "Empty strings list"
+    assert exe_strings <= EXE_STRINGS
+
+
+def test_extract_string_cli(exe_strings: set[str]):
+    exe_file_path = tests_dir / "test64.exe"
     with Path(exe_file_path).open("rb") as pe_file:
         strings = {x.string for x in extract_strings(pe_file)}
-        assert strings >= EXE_STRINGS
+        assert strings >= exe_strings
 
 
-def test_extract_strings_cli_2():
-    exe_file_path = Path(__file__).parent / "test64.exe"
+def test_extract_strings_cli_2(exe_strings: set[str]):
+    exe_file_path = tests_dir / "test64.exe"
     with tempfile.TemporaryDirectory() as temp_dir:
         file_name = Path(temp_dir) / "string_dump.txt"
         with contextlib.suppress(SystemExit):
@@ -77,11 +96,11 @@ def test_extract_strings_cli_2():
         with Path(file_name).open() as file:
             strings = {line.rstrip() for line in file}
 
-        assert strings >= EXE_STRINGS
+        assert strings >= exe_strings
 
 
-def test_extract_strings_cli_3():
-    exe_file_path = Path(__file__).parent / "test64.exe"
+def test_extract_strings_cli_3(exe_strings: set[str]):
+    exe_file_path = tests_dir / "test64.exe"
     stdout = io.StringIO()
 
     with contextlib.redirect_stdout(stdout):
@@ -89,4 +108,4 @@ def test_extract_strings_cli_3():
             main([str(exe_file_path)])
 
         strings = set(stdout.getvalue().splitlines())
-        assert strings >= EXE_STRINGS
+        assert strings >= exe_strings
