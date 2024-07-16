02a3fe86dc497c70648480c6d3e4d73a4399945c
insolor <2442833+insolor@users.noreply.github.com>
2024-07-16 22:40:36 +0300

    Fixes

--- dfint64_patch/backup.py
+++ dfint64_patch/backup.py
@@ -13,7 +13,7 @@ T = TypeVar("T", bound=str | PathLike)
 @contextmanager
 def copy_source_file_context(src: str | PathLike, dest: T, *, cleanup: bool) -> Generator[T, None, None]:
     """
-    Context manager to backup file and restore it on exit (if needed)
+    Context manager to back up file and restore it on exit (if needed)
     """
     logger.info(f"Copying '{src}'\nTo '{dest}'...")
     try:
