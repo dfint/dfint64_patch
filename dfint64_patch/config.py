2fc9d6d33702eaaf5c2bc713808a5ce94f2dd5bc
insolor <2442833+insolor@users.noreply.github.com>
2024-11-07 19:07:45 +0300

    config.py improvements based on code from dfint/search_offsets (#82)

--- dfint64_patch/config.py
+++ dfint64_patch/config.py
@@ -8,9 +8,14 @@ from omegaconf import DictConfig, OmegaConf
 T = TypeVar("T", bound=DictConfig)
 
 
-def with_config(config_class: type[T], config_file: str) -> Callable[[Callable[[T], None]], Callable[[], None]]:
+def with_config(config_class: type[T], *config_files: str) -> Callable[[Callable[[T], None]], Callable[[], None]]:
     """
     A decorator to load the config file and merge it with the CLI options.
+
+    :param config_class: A class, which describes the possible configuration parameters.
+        Should be inherited from `omegaconf.DictConfig`.
+    :param config_files: Names of configs. The closer a file is to the end of the list, the higher its priority.
+        Default config should be the first in the list. CLI options have the highest priority.
     """
 
     def decorator(func: Callable[[T], None]) -> Callable[[], None]:
@@ -18,10 +23,11 @@ def with_config(config_class: type[T], config_file: str) -> Callable[[Callable[[
         def wrapper() -> None:
             config = OmegaConf.structured(config_class)
 
-            try:
-                config.merge_with(OmegaConf.load(config_file))
-            except FileNotFoundError:
-                logger.info(f"Config {config_file} not found, using CLI options only")
+            for config_file in config_files:
+                try:
+                    config.merge_with(OmegaConf.load(config_file))
+                except FileNotFoundError:  # noqa: PERF203
+                    logger.info(f"Config {config_file} not found")
 
             config.merge_with(OmegaConf.from_cli())
 
