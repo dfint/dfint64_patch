794720ec9a302c6d928a42b30c5f8fa1a7d4d382
insolor <2442833+insolor@users.noreply.github.com>
2024-11-07 18:35:57 +0300

    Copypaste config.py code from dfint/search_offsets

--- dfint64_patch/config.py
+++ dfint64_patch/config.py
@@ -1,29 +1,27 @@
 import functools
 from collections.abc import Callable
-from typing import TypeVar
 
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
 
-T = TypeVar("T", bound=DictConfig)
 
-
-def with_config(config_class: type[T], config_file: str) -> Callable[[Callable[[T], None]], Callable[[], None]]:
+def with_config(config_class: type, *config_files: str) -> Callable[[Callable[[DictConfig], None]], Callable[[], None]]:
     """
     A decorator to load the config file and merge it with the CLI options.
     """
 
-    def decorator(func: Callable[[T], None]) -> Callable[[], None]:
+    def decorator(func: Callable[[DictConfig], None]) -> Callable[[], None]:
         @functools.wraps(func)
         def wrapper() -> None:
             config = OmegaConf.structured(config_class)
 
-            try:
-                config.merge_with(OmegaConf.load(config_file))
-            except FileNotFoundError:
-                logger.info(f"Config {config_file} not found, using CLI options only")
-
             config.merge_with(OmegaConf.from_cli())
+
+            for config_file in config_files:
+                try:
+                    config.merge_with(OmegaConf.load(config_file))
+                except FileNotFoundError:  # noqa: PERF203
+                    logger.info(f"Config {config_file} not found, using CLI options only")
 
             missing = ", ".join(OmegaConf.missing_keys(config))
             if missing:
